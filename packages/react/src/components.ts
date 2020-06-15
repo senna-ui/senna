384e877d03875707120980c0bc562aa43ea0a1be
Stefan Zollinger <zos@panter.ch>
2020-06-15 16:23:27 +0200

    chore: add build scripts

--- packages/react/src/components.ts
+++ packages/react/src/components.ts
@@ -8,7 +8,6 @@ import { JSX } from '@senna-ui/core';
 import { defineCustomElements, applyPolyfills } from '@senna-ui/core/loader';
 
 applyPolyfills().then(() => defineCustomElements());
-export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
 export const SaButton = /*@__PURE__*/createReactComponent<JSX.SaButton, HTMLSaButtonElement>('sa-button');
 export const SaCol = /*@__PURE__*/createReactComponent<JSX.SaCol, HTMLSaColElement>('sa-col');
 export const SaContainer = /*@__PURE__*/createReactComponent<JSX.SaContainer, HTMLSaContainerElement>('sa-container');
