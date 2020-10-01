f17490a9656b987ca0aea72d27d0552560c4205d
Stefan Zollinger <zos@panter.ch>
2020-10-01 11:16:46 +0200

    fix: update story for new storybook version

--- core/src/components/input/input.stories.tsx
+++ core/src/components/input/input.stories.tsx
@@ -1,17 +1,11 @@
-import { withActions } from "@storybook/addon-actions";
-
 export default {
   title: "Forms/Input",
 };
 
-const text = () => `<sen-input type="text"></sen-input>`;
-const checkbox = () => `<sen-input name="check" type="checkbox"></sen-input>`;
-const radio = () => `
+export const Text = () => `<sen-input type="text"></sen-input>`;
+export const Checkbox = () => `<sen-input name="check" type="checkbox"></sen-input>`;
+export const Radio = () => `
   <sen-input name="rad" type="radio" value="1"></sen-input> Option 1<br>
   <sen-input name="rad" type="radio" value="2"></sen-input> Option 2<br>
   <sen-input name="rad" type="radio" value="3"></sen-input> Option 3<br>
 `;
-
-export const Text = () => withActions("senInput")(text);
-export const Checkbox = () => withActions("senInput")(checkbox);
-export const Radio = () => withActions("senInput")(radio);
