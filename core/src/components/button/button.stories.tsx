f4d7906da87b5a094c72432a44f1f131e5883813
Kaspar Vollenweider <vok@panter.ch>
2020-10-02 16:01:34 +0200

    feat(button): disabled button has opacity 0.4

--- core/src/components/button/button.stories.tsx
+++ core/src/components/button/button.stories.tsx
@@ -16,3 +16,32 @@ export const Colors = () => `
   </sen-app>
 `;
 
+
+export const Disabled = () => `
+  <sen-app>
+    <sen-button>My button</sen-button>
+    <br><br>
+    <sen-button disabled>My button disabled</sen-button>
+    <br><hr><br>
+    <sen-button color="primary">Primary</sen-button>
+    <br><br>
+    <sen-button color="primary" disabled>Primary disabled</sen-button>
+    <br><hr><br>
+    <sen-button color="secondary">Secondary</sen-button>
+    <br><br>
+    <sen-button color="secondary" disabled>Secondary disabled</sen-button>
+    <br><hr><br>
+    <sen-button color="success">Success</sen-button>
+    <br><br>
+    <sen-button color="success" disabled>Success disabled</sen-button>
+    <br><hr><br>
+    <sen-button color="info">Info</sen-button>
+    <br><br>
+    <sen-button color="info" disabled>Info disabled</sen-button>
+    <br><hr><br>
+    <sen-button color="warning">Warning</sen-button>
+    <br><br>
+    <sen-button color="warning" disabled>Warning disabled</sen-button>
+    <br><br>
+  </sen-app>
+`
