3726c3e84b206b9929e5a3f6a1a71179f2194df4
Kaspar Vollenweider <vok@panter.ch>
2020-10-14 21:18:54 +0200

    fix(button-group): allow round with more than two buttons

--- core/src/components/button-group/button-group.stories.tsx
+++ core/src/components/button-group/button-group.stories.tsx
@@ -8,3 +8,12 @@ export const Default = () => `
     <sen-button color="secondary">Button 2</sen-button>
   </sen-button-group>
 `;
+
+
+export const ThreeButtons = () => `
+  <sen-button-group>
+    <sen-button color="primary">Button 1</sen-button>
+    <sen-button color="secondary">Button 2</sen-button>
+    <sen-button>Button 3</sen-button>
+  </sen-button-group>
+`;
---
+++ core/src/components/button-group/usage/examples.md
@@ -0,0 +1,7 @@
+```html
+<sen-button-group>
+  <sen-button color="primary">Button 1</sen-button>
+  <sen-button color="secondary">Button 2</sen-button>
+  <sen-button>Button 3</sen-button>
+</sen-button-group>
+```
